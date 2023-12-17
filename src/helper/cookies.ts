fd4be4db3cce9af4721c76b29345a1e385857dc1
hiiamtrong <hiiamtrong@gmail.com>
2023-12-17 23:50:47 +0700

    chore: add node_env for each env

--- src/helper/cookies.ts
+++ src/helper/cookies.ts
@@ -1,16 +1,24 @@
-import { VITE_ROOT_DOMAIN } from '@/constants/AppConfig';
-import Cookies from 'js-cookie';
+import { VITE_ROOT_DOMAIN } from "@/constants/AppConfig";
+import Cookies from "js-cookie";
 export class CookiesHelper {
-	static set(name: string, value: string) {
-		const domain = VITE_ROOT_DOMAIN;
-		Cookies.set(name, value, { domain });
-	}
-	static get(name: string) {
-		return Cookies.get(name);
-	}
+  static _getKey(name: string) {
+    const env = process.env.NODE_ENV;
+    if (env === "production") {
+      return name;
+    }
+    return `${env}_${name}`;
+  }
 
-	static remove(name: string) {
-		const domain = VITE_ROOT_DOMAIN;
-		Cookies.remove(name, { domain });
-	}
+  static set(name: string, value: string) {
+    const domain = VITE_ROOT_DOMAIN;
+    Cookies.set(this._getKey(name), value, { domain });
+  }
+  static get(name: string) {
+    return Cookies.get(this._getKey(name));
+  }
+
+  static remove(name: string) {
+    const domain = VITE_ROOT_DOMAIN;
+    Cookies.remove(this._getKey(name), { domain });
+  }
 }

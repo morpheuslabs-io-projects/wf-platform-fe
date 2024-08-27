42a3a5935dcec661234aafc588b1ec0dce7ec567
Ngoc Drakula <ngocdrakula@gmail.com>
2024-08-27 08:22:24 +0700

    fix: no redirect when logout

--- src/pages/Logout/index.tsx
+++ src/pages/Logout/index.tsx
@@ -6,37 +6,29 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 
 export const Logout = () => {
   const { authenticated, logout } = useReactKeycloak();
-  const accessToken = CookiesHelper.get("accessToken");
-  const refreshToken = CookiesHelper.get("refreshToken");
-  const isHasToken = accessToken || refreshToken;
 
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const redirectUrl = searchParams.get("redirect_url");
 
   useEffect(() => {
-    if (authenticated || isHasToken) {
-      handleLogout();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [authenticated, isHasToken]);
+    (async () => {
+      CookiesHelper.clearAll();
+      try {
+        if (authenticated)
+          await logout({
+            redirectUri: redirectUrl || window.location.origin,
+          });
+      } catch (error) {
+        console.log(error);
+      }
+      if (redirectUrl) {
+        window.location.href = redirectUrl;
+      } else {
+        navigate("/");
+      }
+    })();
+  }, [redirectUrl, authenticated, logout, navigate]);
 
-  const handleLogout = async () => {
-    CookiesHelper.remove("accessToken");
-    CookiesHelper.remove("refreshToken");
-    CookiesHelper.remove("userInfo");
-    try {
-      await logout({
-        redirectUri: redirectUrl || window.location.origin,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-    if (redirectUrl) {
-      window.location.href = redirectUrl;
-    } else {
-      navigate("/");
-    }
-  };
   return <Container maxWidth="xl"></Container>;
 };
